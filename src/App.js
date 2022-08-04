e112ffc662f19954e5558f1540462546b48b58fe
Johann Taylor <johann@Johanns-MacBook.local>
2022-08-03 23:15:06 -0400

    Updated styles and scripts for deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from "react";
+import { FaTelegramPlane } from "react-icons/fa"
+import "./style.css";
 
 //======== FIREBASE SET UP ========//
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -21,98 +23,104 @@ firebase.initializeApp({
 // reference auth and firestore SDKs
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-//================================//
+//==============================================================================//
 
 function App() {
-  
   const [user] = useAuthState(auth);
 
   return (
     <>
       <header>
-        <h1>Climbing</h1>
+        <h1>Chat</h1>
         <SignOut />
       </header>
 
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </>
   );
-};
+}
 
+//================ SIGN IN / SIGN OUT ================//
 function SignIn() {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   };
 
-  return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-  );
-};
+  return <button className="login-logout" onClick={signInWithGoogle}>Sign in with Google</button>;
+}
 
 function SignOut() {
-  return auth.currentUser && (
-    <button onClick={()=>auth.signOut()}>Log Out</button>
+  return (
+    auth.currentUser && <button className="login-logout" onClick={() => auth.signOut()}>Log Out</button>
   );
-};
+}
+//=======================================================//
 
 function ChatRoom() {
   const autoScroll = useRef();
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, {idField: 'id'});
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-  const [formValue, setFormValue] = useState('');
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
-  
+
     const { uid, photoURL } = auth.currentUser;
-  
+
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
+      photoURL,
     });
-  
-    setFormValue('');
-    autoScroll.current({ behavior: 'smooth'});
-  }
-  
-  return(
+
+    setFormValue("");
+    autoScroll.current({ behavior: "smooth" });
+  };
+
+  return (
     <>
       <main>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
         <span ref={autoScroll}></span>
       </main>
-     
+
       <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e)=> setFormValue(e.target.value)} placeholder="Message" />
-        <button type="submit" disabled={!formValue}>Send</button>
+        <div className="chat-input">
+          <input
+            type="text"
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            placeholder="Message"
+          />
+        </div>
+        <button className="message-send" type="submit" disabled={!formValue}>
+          <FaTelegramPlane />
+        </button>
       </form>
     </>
   );
-
-
-};
+}
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-
   return (
     <>
       <div className={`message ${messageClass}`}>
-        <img src={photoURL} alt =""/>
-        <p>{text}</p>
+        <img src={photoURL} alt="" />
+        <p>
+          {text}
+        </p>
       </div>
     </>
   );
-};
+}
 
 export default App;
